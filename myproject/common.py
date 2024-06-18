df1153646ea43915385b0010b3766719e9e8b114
Riabokin Ruslan <rriabokin@gmail.com>
2024-06-18 12:15:25 +0300

    Created buttons for selecting a table

---
+++ myproject/common.py
@@ -0,0 +1,60 @@
+from aiogram import F, Router, types
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state
+from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+router = Router()
+
+
+@router.message(Command(commands=["start"]))
+async def cmd_start(message: types.Message, state: FSMContext):
+    """Функция для выбора столиков"""
+    await state.clear()
+
+    # Создаем клавиатуру с кнопками выбора столиков
+    builder = ReplyKeyboardBuilder()
+    for i in range(1, 10):
+        builder.add(types.KeyboardButton(text=f"Стол № {i}"))
+    builder.adjust(3)
+
+    # Отправляем сообщение с клавиатурой
+    await message.answer(
+        text="Выберите столик, нажав на кнопку с номером столика",
+        reply_markup=builder.as_markup(resize_keyboard=True)
+    )
+
+
+
+
+# Нетрудно догадаться, что следующие два хэндлера можно
+# спокойно объединить в один, но для полноты картины оставим так
+
+# default_state - это то же самое, что и StateFilter(None)
+@router.message(StateFilter(None), Command(commands=["cancel"]))
+@router.message(default_state, F.text.lower() == "отмена")
+async def cmd_cancel_no_state(message: Message, state: FSMContext):
+    # Стейт сбрасывать не нужно, удалим только данные
+    await state.set_data({})
+    await message.answer(
+        text="Нечего отменять",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+@router.message(Command(commands=["cancel"]))
+@router.message(F.text.lower() == "отмена")
+async def cmd_cancel(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text="Действие отменено",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+@router.message()
+async def echo(message: types.Message):
+    """Обработчик всех остальных сообщений. Отправляет эхо-ответ с именем и ID пользователя."""
+    await message.answer(f'Привет, {message.from_user.first_name}, твой номер id: {message.from_user.id}')
+    await message.answer(f'Привет, введите команду (/start)')
