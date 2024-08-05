6de2983bf0b1c6e90469ffb474eae98f733bc74d
Riabokin Ruslan <rriabokin@gmail.com>
2024-08-05 17:56:35 +0300

    Created a folder with tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 import os
-from pathlib import Path
 from myproject.database import Database
 
 @pytest.fixture(scope='session', autouse=True)
@@ -9,19 +8,15 @@ def db_conn():
     db = Database("database_test.db")
     db.create_db()  # Создаём структуру базы данных
 
-    print("Соединение с базой данных установлено.")
-
     # Передаём экземпляр Database в тесты
     yield db
 
     # Закрываем соединение и очищаем базу данных после завершения всех тестов
     db.connection.close()
-    print("Соединение с базой данных закрыто.")
 
     # Удаляем файл базы данных
     if os.path.exists("database_test.db"):
         os.remove("database_test.db")
-        print("Файл database_test.db удалён.")
 
 @pytest.fixture(scope='session', autouse=True)
 def check_file_cleanup():
